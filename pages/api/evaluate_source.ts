eb62fe53ca0ab2f44c87ca8871a857d35f1b7fe8
Justin Headley <headley.justin@gmail.com>
2023-04-15 22:54:53 -0700

    chore: small fixes

--- pages/api/evaluate_source.ts
+++ pages/api/evaluate_source.ts
@@ -46,9 +46,9 @@ export type MBTSegmentInfo = {
   end: number;
 };
 
-function timestampToSeconds(timestamp: string): number | null {
+function timestampToSeconds(timestamp?: string): number | null {
   const regex = /(?:([0-9]{1,2}):)?([0-5][0-9]):([0-5][0-9])\.(\d{3})/;
-  const match = timestamp.match(regex);
+  const match = (timestamp || '').match(regex);
 
   if (match) {
     const hours = match[1] ? parseInt(match[1], 10) : 0;
@@ -73,8 +73,8 @@ function extractFirstAndLastTimestamps(text: string): {
     const firstTimestamp = matches[0][0];
     const lastTimestamp = matches[matches.length - 1][0];
     return {
-      first: timestampToSeconds(firstTimestamp),
-      last: timestampToSeconds(lastTimestamp),
+      first: Math.round(timestampToSeconds(firstTimestamp) || 0),
+      last: Math.round(timestampToSeconds(lastTimestamp) || 0),
     };
   } else {
     return { first: null, last: null };
@@ -116,27 +116,27 @@ export const createHashId = (text: string) => {
   }, 0);
 };
 
-function extractVideoId(ytLink: string) {
+function extractVideoId(ytLink?: string) {
   const ytRegex =
     /^(?:https?:\/\/)?(?:www\.)?(?:youtu(?:\.be\/|be\.com\/(?:watch\?v=|embed\/|v\/|shorts\/)))([\w\-]{10,12})(?:[\&\?](?:t=([0-9hms]{1,9}|[0-9]{1,9})))?.*$/;
-  const match = ytLink.match(ytRegex);
+  const match = (ytLink || '').match(ytRegex);
 
   if (match && match[1]) {
     return match[1];
   } else {
-    throw new Error('Invalid YouTube link.');
+    console.log("Invalid YouTube link: ", ytLink)
+    // throw new Error('Invalid YouTube link.');
   }
 }
-function isValidVideoId(videoId: string) {
+function isValidVideoId(videoId?: string) {
   const videoIdRegex = /^[\w-]{10,12}$/;
-  return videoIdRegex.test(videoId);
+  return videoIdRegex.test(videoId || '');
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log('NEW EVALUATION REQUEST', req.body);
   let { userMessage, apiMessage, source_docs } = req.body;
 
   if (!source_docs) {
@@ -161,6 +161,7 @@ export default async function handler(
         } else {
           // ytlink with timestamp of first
           const ytLinkWithTimestamp = ytLink + `&t=${first}`;
+          doc.metadata.ytLink = ytLinkWithTimestamp;
         }
         return doc;
       }),
@@ -182,7 +183,8 @@ export default async function handler(
       const videoId = extractVideoId(doc.metadata.ytLink);
 
       if (!isValidVideoId(videoId)) {
-        throw new Error('Invalid YouTube video ID: ' + videoId);
+        console.log("Invalid YouTube video ID: ", videoId)
+        return null
       }
 
       console.log('FETCHING SEGMENTS BASED ON DOC:', doc);
