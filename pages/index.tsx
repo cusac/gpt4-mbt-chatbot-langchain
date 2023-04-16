67b1f8261868b551f7e1bb07af120d4b499e0295
Justin Headley <headley.justin@gmail.com>
2023-04-15 23:30:46 -0700

    chore: update author

--- pages/index.tsx
+++ pages/index.tsx
@@ -470,8 +470,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://github.com/JKHeadley">
+            Powered by LangChainAI. Demo built by Justin Headley.
           </a>
         </footer>
       </Layout>
