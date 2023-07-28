212b64324637d41a269ac3328ef92c24cae1b666
Justin Headley <headley.justin@gmail.com>
2023-07-27 21:33:18 -0700

    chore: heroku env fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { parsed: localEnv } = require('dotenv').config();
+import dotenv from 'dotenv';
+const localEnv = dotenv.config().parsed;
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
