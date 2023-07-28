f915d5f1aa160a066f775c2ca63142de1be89b8d
Justin Headley <headley.justin@gmail.com>
2023-07-27 21:31:53 -0700

    chore: heroku env fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const { parsed: localEnv } = require('dotenv').config();
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
+  },
+  env: {
+    NEXT_PUBLIC_ENV: 'development',
   },
   typescript: {
     // !! WARN !!
